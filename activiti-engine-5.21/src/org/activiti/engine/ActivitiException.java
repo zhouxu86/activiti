dd6bd8d527ce3195b4cd57479b2e28077e1d961c
zhouxu86 <aaron@172.25.13.120>
2017-02-27 14:35:49 +0800

    test git

--- activiti-engine-5.21/src/org/activiti/engine/ActivitiException.java
+++ activiti-engine-5.21/src/org/activiti/engine/ActivitiException.java
@@ -16,6 +16,8 @@ package org.activiti.engine;
 /**
  * Runtime exception that is the superclass of all Activiti exceptions.
  * 
+ * 
+ * 
  * @author Tom Baeyens
  */
 public class ActivitiException extends RuntimeException {
